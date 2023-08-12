13cffe22d0c3ddc6ffabe641c675b67d6fb36244
Dimitri-Y <dimoon.bond@gmail.com>
2023-08-12 14:40:48 +0300

    delete comment code

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -33,39 +33,4 @@ Modal.propTypes={
     largeImageURL: PropTypes.string.isRequired,
     alt: PropTypes.string.isRequired,  
 }
-// class Modal extends Component {
-//   static propTypes = {
-//     onCloseModal: PropTypes.func.isRequired,
-//     largeImageURL: PropTypes.string.isRequired,
-//     alt: PropTypes.string.isRequired,  
-//   }  
-//   handleKeyDown = event => {
-//         if (event.code === 'Escape') {
-//           this.props.onCloseModal();
-//         }
-//       };
-    
-//       handleOverlayClick = event => {
-//         if (event.currentTarget === event.target) {
-//           this.props.onCloseModal();
-//         }
-//       };
-    
-//       componentDidMount() {
-//         window.addEventListener('keydown', this.handleKeyDown);
-//       }
-    
-//       componentWillUnmount() {
-//         window.removeEventListener('keydown', this.handleKeyDown);
-//       }
-//     render(){
-//         const {largeImageURL,alt}=this.props;
-//         return(
-//             <div className={css["Overlay"]} onClick={this.handleOverlayClick}>
-//                 <div className={css["Modal"]}>
-//                     <img src={largeImageURL} alt={alt} />
-//                 </div>    
-//             </div>);
-//     }
-// }
 export default Modal;
